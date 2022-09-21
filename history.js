a7087a663cc9e8c3f7c42e4aa69857bcdab428e8
ahasannahid <ahasan15-9072@diu.edu.bd>
2022-09-22 01:59:36 +0600

    first commit

---
+++ history.js
@@ -0,0 +1,43 @@
+const histories = document.getElementById("histories");
+
+function addHistory(questionText, timeTakenRound, errorCount, wordPerMmunite) {
+  const newRow = document.createElement("div");
+  newRow.classList.add("card");
+
+  newRow.innerHTML = `
+  <h3>${questionText}</h3>
+  <div>
+  <p>You took: <span class="bold">${timeTakenRound}</span> seconds</p>
+  <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
+  </div>
+  `;
+
+  histories.appendChild(newRow);
+
+  let previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
+  previousTests.push({ questionText, timeTakenRound, errorCount, wordPerMmunite });
+  localStorage.setItem("testHistory", JSON.stringify(previousTests));
+
+  displayHistory();
+}
+
+function displayHistory() {
+  histories.innerHTML = "";
+  const previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
+
+  previousTests.forEach((test) => {
+    const newRow = document.createElement("div");
+    newRow.classList.add("card");
+
+    newRow.innerHTML = `
+    <div>
+  <h3>${test.questionText}</h3>
+  <p>You took: <span class="bold">${test.timeTakenRound}</span> seconds</p>
+    <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
+    <p>You Typing speed <span class="bold red">${test.wordPerMmunite}</span> WPM</p>
+    </div>
+  `;
+
+    histories.appendChild(newRow);
+  });
+}
